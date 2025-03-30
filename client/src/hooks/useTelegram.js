be11782fa757a1ffc4bdf074d1cc8473aa932672
vas-alina <vasalina93@gmail.com>
2025-03-30 13:49:04 +0300

    fixed

--- client/src/hooks/useTelegram.js
+++ client/src/hooks/useTelegram.js
@@ -1,24 +1,22 @@
 const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
-
     const onClose = () => {
-        tg.close()
-    }
+        tg.close();
+    };
 
     const onToggleButton = () => {
         if (tg.MainButton.isVisible) {
             tg.MainButton.hide();
-
         } else {
             tg.MainButton.show();
         }
+    };
 
-        return {
-            onClose,
-            onToggleButton,
-            tg,
-            user: tg.initDataUnsafe?.user
-        }
-    }
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user
+    };
 }
