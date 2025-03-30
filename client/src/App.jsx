066de862b5ace55cce52069a1b1d5e78da123a24
vas-alina <vasalina93@gmail.com>
2025-03-30 13:41:47 +0300

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import { useEffect } from 'react';
-import { useTelegram } from './hooks/useTelegram';
-import './App.css'
-
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import "./App.css";
 
 function App() {
-  const {tg, onToggleButton} = useTelegram();
+  const { tg, onToggleButton } = useTelegram();
 
   useEffect(() => {
     tg.ready();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
+      <Header />
       <button onClick={onToggleButton}>toggle</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/hooks/useTelegram.js
+++ client/src/hooks/useTelegram.js
@@ -4,20 +4,21 @@ export function useTelegram() {
 
     const onClose = () => {
         tg.close()
-      }
+    }
 
     const onToggleButton = () => {
         if (tg.MainButton.isVisible) {
             tg.MainButton.hide();
-          
+
         } else {
             tg.MainButton.show();
         }
 
-      return {
-        onClose,
-        onToggleButton,
-        tg,
-        user: tg.initDataUnsafe?.user
-      }
+        return {
+            onClose,
+            onToggleButton,
+            tg,
+            user: tg.initDataUnsafe?.user
+        }
+    }
 }
